8e60a95cb5a978618ccdf65e11a52e7782cccf01
Louis Becquey <Louis Becquey>
2017-02-16 11:21:51 +0100

    Classes fromat de base

--- src/Population.cpp
+++ src/Population.cpp
@@ -1,3 +1,28 @@
-#include <Population.h>
+//============================ INCLUDES ================================
+#include "Population.h"
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+//========================= STATIC ATTRIBUTES ==========================
 
 
+//=========================== CONSTRUCTORS =============================
+Population::Population()
+{
+	
+}
+
+//=========================== DESTRUCTORS ==============================
+Population::~Population()
+{
+	
+}
+
+
+//=========================== PUBLIC METHODS ===========================
+
+//========================== PROTECTED METHODS =========================
+
+//============================ FUNCTIONS ===============================
