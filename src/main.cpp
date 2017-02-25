0348c5761af81988e6e07b83c99bc40adc9a4f5d
Louis Becquey <Louis Becquey>
2017-02-25 17:51:51 +0100

    petites modifications esthétiques

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include "Population.h"
-#include <cstdlib>
-#include <SFML/Graphics.hpp>
 
 using namespace sf;
 
 int	main(void)
 {
-	RenderWindow	window( VideoMode(500, 500), "Resulting biological network");
-    CircleShape 	circle(10.f);
     unsigned int	iter(0);
-    unsigned int	Ngraphs, Nnodes, Nedges;
+    unsigned int	Ngraphs, Nnodes, Nedges, itermax;
     
+    //===================== PARAMETERS DEFININTION =====================
+    itermax	=	1;
     
     //===================== POPULATION GENERATION ======================
-	Ngraphs = 100;
-	Nnodes = 100;
-	Nedges = 300;
+	Ngraphs	= 	100;
+	Nnodes	= 	100;
+	Nedges 	= 	300;
 	Population	experiment1 = Population(Ngraphs, Nnodes, Nedges);
 	printf("Creation of %d graphs completed, with %d nodes and %d edges in each.\n", Ngraphs, Nnodes, Nedges);
 	printf("The nodes were built with a power law degree distribution, which power parameter is %f.\n", Graph::LAW_EXPONENT);
