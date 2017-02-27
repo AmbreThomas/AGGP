33248467b61398029228d1a203ab42a3cab87abb
Louis Becquey <Louis Becquey>
2017-02-27 12:44:10 +0100

    Time ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,48 @@
 
 using namespace sf;
 
-void	printTime_str(void);
+void	printTime_str(const struct tm*);
+void	printDiffTime_str(int);
 
 int		main(void)
 {
     unsigned int	iter(0);
     unsigned int	Ngraphs, Nnodes, Nedges, itermax;
+	time_t			startTime, endTime;
 	
     srand((unsigned int)time(NULL));
-	
+    startTime	=	time(NULL);
     
     //===================== PARAMETERS DEFININTION =====================
-    itermax	=	100;
+    itermax	=	10;
     
     //===================== POPULATION GENERATION ======================
-	Ngraphs	= 	1000;
+	Ngraphs	= 	100;
 	Nnodes	= 	1000;
 	Nedges 	= 	3000;
 	Population	experiment1 = Population(Ngraphs, Nnodes, Nedges);
 	printf("Creation of %d graphs completed, with %d nodes and %d edges in each.\n", Ngraphs, Nnodes, Nedges);
 	printf("The nodes were built with a power law degree distribution, which power parameter is %f.\n\n", Graph::LAW_EXPONENT);
-	printf("=============================================================================================\n");
-	printTime_str();
-	printf(" : Expect end of computation in %d minutes.\n\n", (int)Nedges/1000*itermax);
+	printf("\n");
+	printTime_str(localtime(&startTime));
+	printf(" : Expect end of computation in %d minutes.\n\n", 
+			(int)Nedges/1000000*Ngraphs*itermax);
+	
 	//===================== MAIN LOOP ==================================
 	while (iter<itermax)
 	{
-		printf("  Iteration %d: pop of size %d\n", iter++, (int)experiment1.getCurrentSize());
+		printf("  Iteration %d: pop of size %d\n",
+				++iter, (int)experiment1.getCurrentSize());
 		experiment1.cross(); 				//population size N ==> 2N
 		experiment1.select_by_tournament();	//population size 2N ==> N
 		//~ experiment1.select_elite();			//population size 2N ==> N
 	}
-
+	
+	endTime	=	time(NULL);
+	printf("\nComputed in ");
+	printDiffTime_str((int) difftime(endTime, startTime));
+	printf(", displaying best solution...\n");
+	
 	//===================== DISPLAY RESULTS ============================
 	Graph best_graph = experiment1.getgraph(0);
 	best_graph.compute_layout();
@@ -48,9 +58,9 @@ int		main(void)
 				case Event::Closed:
 					window.close();
 					break;
-				//~ case Event::Resized:
-					//~ window.setView(View(FloatRect(0,0, (float)event.size.width, (float)event.size.height)));
-					//~ break;
+				case Event::Resized:
+					window.setView(View(FloatRect(0,0, (float)event.size.width, (float)event.size.height)));
+					break;
 				default:
 					break;
 			}
@@ -63,14 +73,24 @@ int		main(void)
 	return 0;
 }
 
-void	printTime_str(void)
+void	printTime_str(const struct tm *timeptr)
 {
-    time_t 			rawtime;
-	struct tm*	 	timeinfo;
-	char 			buffer[80];
-	
-	time (&rawtime);
-	timeinfo = localtime (&rawtime);
-	strftime (buffer,80,"%I:%M%p.",timeinfo);
-	printf("%s",buffer);
+	static const char 	wday_name[][4] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+	static const char 	mon_name[][4] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+	printf("%.3s%3d %.3s %d | %.2dh %.2dm %.2ds", 
+			wday_name[timeptr->tm_wday],
+			timeptr->tm_mday, mon_name[timeptr->tm_mon], 1900 + timeptr->tm_year,
+			timeptr->tm_hour, timeptr->tm_min, timeptr->tm_sec);
+}
+
+void	printDiffTime_str(int diff)
+{
+	int	days	=	(int) diff/86400;
+	int	hours	=	(int) diff/3600;
+	int	min		=	(int) diff/60;
+	int	sec		=	(int) (diff - diff/60);
+	if (days) 	printf("%dd ", days);
+	if (hours) 	printf("%d° ", hours);
+	if (min) 	printf("%d' ", min);
+	printf("%d\" ", sec);
 }
